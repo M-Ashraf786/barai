25940d8e68b8d7b014aaec1edb75bd59e51f3ce7
Muhammad Ashraf <commonuse100@gmail.com>
2025-02-13 22:33:50 +0500

    Fix Vercel function runtime issue

--- backend/index.js
+++ backend/index.js
@@ -1,61 +1,14 @@
-// const express = require('express');
-// const cors = require('cors');
-// const cookieParser = require('cookie-parser');
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const serverless = require("serverless-http"); // Add this for Vercel support
 
-// const connectDb = require('./config/db')
-// require('dotenv').config()
+const connectDb = require("./config/db");
+require("dotenv").config();
 
-//   const userRoute = require('./routes/userRoute');
-//   const postRoute = require('./routes/postRoute');
-//   const memberRoute = require('./routes/memberRoute')
-
-
-// const app = express();
-// app.use(express.json());
-// app.use(cookieParser());
-
-// const corsOptions = {
-//     origin:process.env.FRONTEND_URL,
-//     credentials:true,
-  
-//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//     allowedHeaders: [
-//       "Content-Type",
-//       "Authorization",
-//       "Cache-Control",
-//       "pragma", // Added pragma to allowed headers
-//     ],
-//     exposedHeaders: ["Content-Range", "X-Content-Range"],
-//     preflightContinue: false,
-//     optionsSuccessStatus: 204,
-// }
-// app.use(cors(corsOptions))
-
-// connectDb();
-
-//  app.use('/userCollection', userRoute);
- 
-//  app.use('/postCollection', postRoute);
-//  app.use('/members', memberRoute)
-
-
-// const PORT = process.env.PORT || 8000;
-// app.listen(PORT, ()=>console.log(`App is running on ${PORT}` ));
-
-
-//
-
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const serverless = require('serverless-http'); // Import serverless-http
-
-const connectDb = require('./config/db');
-require('dotenv').config();
-
-const userRoute = require('./routes/userRoute');
-const postRoute = require('./routes/postRoute');
-const memberRoute = require('./routes/memberRoute');
+const userRoute = require("./routes/userRoute");
+const postRoute = require("./routes/postRoute");
+const memberRoute = require("./routes/memberRoute");
 
 const app = express();
 app.use(express.json());
@@ -75,10 +28,11 @@ app.use(cors(corsOptions));
 connectDb();
 
 // Define routes
-app.use('/userCollection', userRoute);
-app.use('/postCollection', postRoute);
-app.use('/members', memberRoute);
+app.use("/userCollection", userRoute);
+app.use("/postCollection", postRoute);
+app.use("/members", memberRoute);
 
-// Export as a serverless function
+// Export the server as a serverless function
 module.exports = app;
 module.exports.handler = serverless(app);
+
