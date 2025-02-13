98f6239b253f0e13ef8331a55d14d4f0bfe6e55d
Muhammad Ashraf <commonuse100@gmail.com>
2025-02-13 23:42:20 +0500

    Moved index.js to api/index.js for Vercel compatibility

---
+++ backend/api/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const serverless = require("serverless-http");
+
+const connectDb = require("../config/db"); // Update path
+require("dotenv").config();
+
+const userRoute = require("../routes/userRoute");
+const postRoute = require("../routes/postRoute");
+const memberRoute = require("../routes/memberRoute");
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+
+const corsOptions = {
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "Cache-Control", "pragma"],
+    exposedHeaders: ["Content-Range", "X-Content-Range"],
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+};
+app.use(cors(corsOptions));
+
+connectDb();
+
+// Define routes
+app.use("/userCollection", userRoute);
+app.use("/postCollection", postRoute);
+app.use("/members", memberRoute);
+
+// Export for Vercel
+module.exports = app;
+module.exports.handler = serverless(app);
+
+
+
